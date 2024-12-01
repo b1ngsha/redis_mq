59bb302c348534a56c1289411e4bce46428b1d9e
陈梓淳 <2941125807@qq.com>
2024-12-01 14:45:37 +0800

    feat: add consumer options

--- redis_mq/options.go
+++ redis_mq/options.go
@@ -1,5 +1,7 @@
 package redis_mq
 
+import "time"
+
 const (
 	// client
 	DefaultMaxIdle            = 20
@@ -8,6 +10,13 @@ const (
 
 	// producer
 	DefaultMaxMsgLen = 500
+
+	// consumer
+	DefaultConsumeTimeout           = 2 * time.Second
+	DefaultMaxRetryLimit            = 3
+	DefaultDeadLetterQueue          = NewDeadLetterQueue()
+	DefaultDeadLetterDeliverTimeout = time.Second
+	DefaultHandleMsgsTimeout        = time.Second
 )
 
 type ClientOptions struct {
@@ -77,3 +86,63 @@ func repairProducer(opts *ProducerOptions) {
 		opts.maxMsgLen = DefaultMaxMsgLen
 	}
 }
+
+type ConsumerOptions struct {
+	consumeTimeout           time.Duration // blocking consume every new message timeout
+	maxRetryLimit            int           // max retry times for each message, if exceed, the message will be pushed to dead letter queue
+	deadLetterQueue          DeadLetterQueue
+	deadLetterDeliverTimeout time.Duration
+	handleMsgsTimeout        time.Duration
+}
+
+type ConsumerOption func(opts *ConsumerOptions)
+
+func WithConsumeTimeout(consumeTimeout time.Duration) ConsumerOption {
+	return func(opts *ConsumerOptions) {
+		opts.consumeTimeout = consumeTimeout
+	}
+}
+
+func WithMaxRetryLimit(maxRetryLimit int) ConsumerOption {
+	return func(opts *ConsumerOptions) {
+		opts.maxRetryLimit = maxRetryLimit
+	}
+}
+
+func WithDeadLetterQueue(deadLetterQueue DeadLetterQueue) ConsumerOption {
+	return func(opts *ConsumerOptions) {
+		opts.deadLetterQueue = deadLetterQueue
+	}
+}
+
+func WithDeadLetterDeliverTimeout(deadLetterDeliverTimeout time.Duration) ConsumerOption {
+	return func(opts *ConsumerOptions) {
+		opts.deadLetterDeliverTimeout = deadLetterDeliverTimeout
+	}
+}
+
+func WithHandleMsgsTimeout(handleMsgsTimeout time.Duration) ConsumerOption {
+	return func(opts *ConsumerOptions) {
+		opts.handleMsgsTimeout = handleMsgsTimeout
+	}
+}
+
+func repairConsumer(opts *ConsumerOptions) {
+	if opts.consumeTimeout < 0 {
+		opts.consumeTimeout = 2 * time.Second
+	}
+	if opts.maxRetryLimit < 0 {
+		opts.maxRetryLimit = DefaultMaxRetryLimit
+	}
+	if opts.deadLetterQueue == nil {
+		opts.deadLetterQueue = DefaultDeadLetterQueue
+	}
+	if opts.deadLetterDeliverTimeout <= 0 {
+		opts.deadLetterDeliverTimeout = DefaultDeadLetterDeliverTimeout
+	}
+	if opts.handleMsgsTimeout <= 0 {
+		opts.handleMsgsTimeout = DefaultHandleMsgsTimeout
+	}
+}
+
+
